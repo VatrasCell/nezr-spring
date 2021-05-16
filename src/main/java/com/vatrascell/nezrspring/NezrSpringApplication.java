91293683d99276591aee6d4668ec617020e66cd6
VatrasCell <vatrascell@googlemail.com>
2021-05-16 14:16:36 +0200

    init

---
+++ src/main/java/com/vatrascell/nezrspring/NezrSpringApplication.java
@@ -0,0 +1,13 @@
+package com.vatrascell.nezrspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NezrSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NezrSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vatrascell/nezrspring/NezrSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vatrascell.nezrspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NezrSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
